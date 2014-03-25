f7f5350682a66fb57209513f0151d59639f84dea
Jason Knebel <jasonkneb@gmail.com>
2014-03-25 15:48:36 -0400

    replacing bright tags with google tag manager

--- app/overrides/google_tags.rb
+++ app/overrides/google_tags.rb
@@ -1,30 +1,30 @@
 # Insert on cart page
 Deface::Override.new(:virtual_path => "spree/orders/edit",
-	                   :name => "brighttag_cart",
+	                   :name => "googletags_cart",
 	                   :insert_before => "h1",
-	                   :partial => "spree/brighttag_tags/cart",
+	                   :partial => "spree/google_tags/cart",
 	                   :disabled => false)
 
 # Insert on order summary page 
 Deface::Override.new(:virtual_path => "spree/orders/show",
-	                   :name => "brighttag_order_summary",
+	                   :name => "googletags_order_summary",
 	                   :insert_before => "#order",
-	                   :partial => "spree/brighttag_tags/orders_show",
+	                   :partial => "spree/google_tags/orders_show",
 	                   :disabled => false)
 	                   
 # Insert on taxon page - taxon dependency is handled in the partial
 Deface::Override.new(:virtual_path => "spree/taxons/show",
-	                   :name => "brighttag_taxon_show",
+	                   :name => "googletags_taxon_show",
 	                   :insert_before => ".taxon-title",
-	                   :partial => "spree/brighttag_tags/taxon_show",
+	                   :partial => "spree/google_tags/taxon_show",
 	                   :disabled => false)
 
 
 # Insert on product page
 Deface::Override.new(:virtual_path => "spree/products/show",
-	                   :name => "brighttag_product_show",
+	                   :name => "googletags_product_show",
 	                   :insert_before => ".product-title",
-	                   :partial => "spree/brighttag_tags/product_show",
+	                   :partial => "spree/google_tags/product_show",
 	                   :disabled => false)
 
 

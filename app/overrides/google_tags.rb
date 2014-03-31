38895b0997d4c69343d784fe05b99ceb8a111ac2
Jason Knebel <jasonkneb@gmail.com>
2014-03-31 14:24:06 -0400

    adding GTM controller logic

--- app/overrides/google_tags.rb
+++ app/overrides/google_tags.rb
@@ -1,30 +1,27 @@
 # Insert on cart page
-Deface::Override.new(:virtual_path => "spree/orders/edit",
-	                   :name => "googletags_cart",
-	                   :insert_before => "h1",
-	                   :partial => "spree/google_tags/cart",
-	                   :disabled => false)
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "googletags_cart",
+                     :insert_top => "body",
+                     :partial => "spree/google_tags/cart",
+                     :disabled => false)
 
 # Insert on order summary page 
-Deface::Override.new(:virtual_path => "spree/orders/show",
-	                   :name => "googletags_order_summary",
-	                   :insert_before => "#order",
-	                   :partial => "spree/google_tags/orders_show",
-	                   :disabled => false)
-	                   
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "googletags_order_summary",
+                     :insert_top => "body",
+                     :partial => "spree/google_tags/orders_show",
+                     :disabled => false)
+                   
 # Insert on taxon page - taxon dependency is handled in the partial
-Deface::Override.new(:virtual_path => "spree/taxons/show",
-	                   :name => "googletags_taxon_show",
-	                   :insert_before => ".taxon-title",
-	                   :partial => "spree/google_tags/taxon_show",
-	                   :disabled => false)
-
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "googletags_taxon_show",
+                     :insert_top => "body",
+                     :partial => "spree/google_tags/taxon_show",
+                     :disabled => false)
 
 # Insert on product page
-Deface::Override.new(:virtual_path => "spree/products/show",
-	                   :name => "googletags_product_show",
-	                   :insert_before => ".product-title",
-	                   :partial => "spree/google_tags/product_show",
-	                   :disabled => false)
-
-
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "googletags_product_show",
+                     :insert_top => "body",
+                     :partial => "spree/google_tags/product_show",
+                     :disabled => false)

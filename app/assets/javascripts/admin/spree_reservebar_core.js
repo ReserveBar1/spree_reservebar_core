2ff0f7d0f162a90615676b2437d832da033e32c0
Jason Knebel <jasonkneb@gmail.com>
2014-04-10 13:47:37 -0400

    adding engaving option

--- app/assets/javascripts/admin/spree_reservebar_core.js
+++ app/assets/javascripts/admin/spree_reservebar_core.js
@@ -1,3 +1,15 @@
 //= require admin/spree_core
 //= require admin/spree_auth
 //= require_tree .
+
+// Toggle Engraving
+$(function(){
+  $('#product_engravable').change(function() {
+    if( $(this).attr('checked') == 'checked') {
+      // Show engraving options
+    }
+    else {
+      // Hide engraving options
+    }
+  });
+});

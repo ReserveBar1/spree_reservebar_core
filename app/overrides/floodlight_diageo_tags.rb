1a6291e2cafc5dc948caea15d3d8f8f575d3268e
Jason Knebel <jasonkneb@gmail.com>
2014-04-10 17:45:15 -0400

    renaming diageo tags

--- app/overrides/floodlight_diageo_tags.rb
+++ app/overrides/floodlight_diageo_tags.rb
@@ -1,27 +1,27 @@
 # Insert on cart page
 # Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
-#                      :name => "diageotags_cart",
+#                      :name => "floodlight_diageo_cart",
 #                      :insert_top => "body",
 #                      :partial => "spree/floodlight_tags/diageo/cart",
 #                      :disabled => false)
 
 # Insert on order summary page 
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
-                     :name => "diageotags_order_summary",
+                     :name => "floodlight_diageo_order_summary",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/diageo/orders_show",
                      :disabled => false)
                    
 # Insert on taxon page - taxon dependency is handled in the partial
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
-                     :name => "diageotags_taxon_show",
+                     :name => "floodlight_diageo_taxon_show",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/diageo/taxon_show",
                      :disabled => false)
 
 # Insert on product page
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
-                     :name => "diageotags_product_show",
+                     :name => "floodlight_diageo_product_show",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/diageo/product_show",
                      :disabled => false)

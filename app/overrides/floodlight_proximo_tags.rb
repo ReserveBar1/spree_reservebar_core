f4cbf0a0bfc341bdafdfab9820e51472b54ef563
Jason Knebel <jasonkneb@gmail.com>
2014-04-10 18:02:52 -0400

    adding proximo tags to registration page

--- app/overrides/floodlight_proximo_tags.rb
+++ app/overrides/floodlight_proximo_tags.rb
@@ -4,3 +4,10 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/proximo/cart",
                      :disabled => false)
+
+# Insert on checkout registration page
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "floodlight_proximo_registration",
+                     :insert_top => "body",
+                     :partial => "spree/floodlight_tags/proximo/registration",
+                     :disabled => false)

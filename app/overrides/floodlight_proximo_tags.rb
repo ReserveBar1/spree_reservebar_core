6fc533719346d43ce238fc88d59e0747a638d0c4
Jason Knebel <jasonkneb@gmail.com>
2014-04-17 02:08:18 -0400

    adding floodlight tag to RDLF product page

--- app/overrides/floodlight_proximo_tags.rb
+++ app/overrides/floodlight_proximo_tags.rb
@@ -33,9 +33,16 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :partial => "spree/floodlight_tags/proximo/dobel_product",
                      :disabled => false)
 
-# Insert on Jose Cuervo® Reserva de la Familia® page
+# Insert on Jose Cuervo® Reserva de la Familia® brand page
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
-                     :name => "floodlight_proximo_rdlf",
+                     :name => "floodlight_proximo_rdlf_brand",
                      :insert_top => "body",
-                     :partial => "spree/floodlight_tags/proximo/rdlf",
+                     :partial => "spree/floodlight_tags/proximo/rdlf_brand",
+                     :disabled => false)
+
+# Insert on Jose Cuervo® Reserva de la Familia® product page
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "floodlight_proximo_rdlf_product",
+                     :insert_top => "body",
+                     :partial => "spree/floodlight_tags/proximo/rdlf_product",
                      :disabled => false)

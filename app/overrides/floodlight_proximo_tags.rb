9411bab649ac8464557c28843345d24d15b5543f
Jason Knebel <jasonkneb@gmail.com>
2014-04-16 20:14:24 -0400

    adding tag to dobel product page

--- app/overrides/floodlight_proximo_tags.rb
+++ app/overrides/floodlight_proximo_tags.rb
@@ -19,11 +19,18 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :partial => "spree/floodlight_tags/proximo/confirmation",
                      :disabled => false)
 
-# Insert on MAESTRO DOBEL® page
+# Insert on MAESTRO DOBEL® brand page
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
-                     :name => "floodlight_proximo_dobel",
+                     :name => "floodlight_proximo_dobel_brand",
                      :insert_top => "body",
-                     :partial => "spree/floodlight_tags/proximo/dobel",
+                     :partial => "spree/floodlight_tags/proximo/dobel_brand",
+                     :disabled => false)
+
+# Insert on MAESTRO DOBEL® product page
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "floodlight_proximo_dobel_product",
+                     :insert_top => "body",
+                     :partial => "spree/floodlight_tags/proximo/dobel_product",
                      :disabled => false)
 
 # Insert on Jose Cuervo® Reserva de la Familia® page

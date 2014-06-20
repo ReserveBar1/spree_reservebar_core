0ea60f1b3cd23bb3d627f5ee2c627ae079c3061a
Jason Knebel <jasonkneb@gmail.com>
2014-06-20 08:15:47 -0400

    adding confirmation SF tag

--- app/overrides/floodlight_hennessy.rb
+++ app/overrides/floodlight_hennessy.rb
@@ -18,3 +18,10 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/hennessy/SF_checkout",
                      :disabled => false)
+
+# Insert at confirmation for V.S Shepard Fairey products
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "floodlight_hennessy_SF_confirmation",
+                     :insert_top => "body",
+                     :partial => "spree/floodlight_tags/hennessy/SF_confirmation",
+                     :disabled => false)

c6f1ff115cc0eacb5880ea05a75a10c9b496385a
Jason Knebel <jasonkneb@gmail.com>
2014-07-08 16:57:02 -0400

    adding product page tags

--- app/overrides/floodlight_glenmorangie.rb
+++ app/overrides/floodlight_glenmorangie.rb
@@ -4,3 +4,10 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/glenmorangie/brand",
                      :disabled => false)
+
+# Insert on Glenmorangie product pages
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "floodlight_glenmorangie_products",
+                     :insert_top => "body",
+                     :partial => "spree/floodlight_tags/glenmorangie/products",
+                     :disabled => false)

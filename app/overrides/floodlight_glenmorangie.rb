beda26b22127b198e8d1e5dcc513ba5c3e988075
Jason Knebel <jasonkneb@gmail.com>
2014-07-08 17:11:28 -0400

    adding for cart

--- app/overrides/floodlight_glenmorangie.rb
+++ app/overrides/floodlight_glenmorangie.rb
@@ -11,3 +11,10 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/glenmorangie/products",
                      :disabled => false)
+
+# Insert on Cart page, if there are Glenmorangie products
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "floodlight_glenmorangie_cart",
+                     :insert_top => "body",
+                     :partial => "spree/floodlight_tags/glenmorangie/cart",
+                     :disabled => false)

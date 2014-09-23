ebdcab197f695e5b5f81628ebd8a0284d1d29286
Kenton Newby <kenton@railsdog.com>
2014-09-23 15:37:21 -0400

    allows admin to upload csv file to mass update product costs for retailers

--- app/models/spree/product_cost.rb
+++ app/models/spree/product_cost.rb
@@ -5,11 +5,32 @@ module Spree
   class ProductCost < ActiveRecord::Base
     belongs_to :retailer
     belongs_to :variant
-    
-    
+
+
     def shipping_surcharge_is_not_zero
       where('shipping_surcharge_is_not_zero > 0')
     end
-    
+
+    def self.import(file)
+      CSV.foreach(file, headers: true) do |row|
+        h = row.to_hash
+
+        begin
+          retailer = Spree::Retailer.find_by_name(h["retailer_name"])
+          variant  = Spree::Variant.find_by_sku(h["sku"])
+          product_cost = Spree::ProductCost.find_by_retailer_id_and_variant_id(retailer.id, variant.id) if retailer.present? && variant.present?
+
+          if product_cost.present?
+            product_cost.cost_price         = h["cost"].to_f               if h["cost"].present?
+            product_cost.shipping_surcharge = h["shipping_surcharge"].to_f if h["shipping_surcharge"].present?
+            product_cost.fulfillment_fee    = h["fulfillment_fee"].to_f    if h["fulfillment_fee"].present?
+            product_cost.save
+          end
+        rescue => e
+          Rails.log e
+        end
+      end
+    end
+
   end
 end

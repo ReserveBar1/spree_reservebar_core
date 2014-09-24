0411d292cbb71b2a114fe29e5d65d31cccd5d2c7
Jason Knebel <jasonkneb@gmail.com>
2014-09-24 15:34:55 +0200

    updates to try to fix report mailers not going out
    * keeps "inline"
    * removes "multipart/mixed"

--- app/mailers/profit_and_loss_report_mailer.rb
+++ app/mailers/profit_and_loss_report_mailer.rb
@@ -14,7 +14,7 @@ class ProfitAndLossReportMailer < ActionMailer::Base
     end
     @search_params = search_params
 
-    attachments["profit_loss_report.csv"] = report_csv_file
+    attachments.inline["profit_loss_report.csv"] = report_csv_file
     mail(:to => @current_user.email, :reply_to => "noreply@reservebar.com", :subject => "Your profit/loss total report is ready.")
   end
 

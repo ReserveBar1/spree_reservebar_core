7f34a9bc08969e3b9fbdd5bcf851cf5e1109b319
Jason Knebel <jasonkneb@gmail.com>
2014-10-24 16:13:17 +0200

    adding tags to belvedere product pages

--- app/overrides/floodlight_belvedere_tags.rb
+++ app/overrides/floodlight_belvedere_tags.rb
@@ -4,3 +4,10 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/belvedere/brand",
                      :disabled => false)
+
+# Insert on Belvedere product pages
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "floodlight_belvedere_products",
+                     :insert_top => "body",
+                     :partial => "spree/floodlight_tags/belvedere/products",
+                     :disabled => false)

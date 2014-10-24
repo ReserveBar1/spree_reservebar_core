a4e1c71cf0e5daa74c3b57f0cf37e0d5fbf0b7dc
Jason Knebel <jasonkneb@gmail.com>
2014-10-24 16:59:11 +0200

    adding belvedere tags to address step

--- app/overrides/floodlight_belvedere_tags.rb
+++ app/overrides/floodlight_belvedere_tags.rb
@@ -11,3 +11,10 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/belvedere/products",
                      :disabled => false)
+
+# Insert on Address page, if there are Belvedere products
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "floodlight_belvedere_address",
+                     :insert_top => "body",
+                     :partial => "spree/floodlight_tags/belvedere/address",
+                     :disabled => false)

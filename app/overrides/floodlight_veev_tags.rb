e6a6ccca45266b67777d5d663cb1d48357d2b126
Jason Knebel <jasonkneb@gmail.com>
2014-10-30 10:21:27 +0100

    adding VEEV tag to product pages

--- app/overrides/floodlight_veev_tags.rb
+++ app/overrides/floodlight_veev_tags.rb
@@ -5,6 +5,13 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :partial => "spree/floodlight_tags/veev/brand",
                      :disabled => false)
 
+# Insert on Glenmorangie product pages
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "floodlight_veev_products",
+                     :insert_top => "body",
+                     :partial => "spree/floodlight_tags/veev/products",
+                     :disabled => false)
+
 # Insert on Confirmation page, if there are VEEV products
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "floodlight_veev_confirmation",

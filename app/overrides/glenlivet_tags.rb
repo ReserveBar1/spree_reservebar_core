5320bceed020d464078c82c4d3dbe8be409f07b1
Jason Knebel <jasonkneb@gmail.com>
2014-11-14 13:47:36 +0100

    adding tags to Glenlivet cart

--- app/overrides/glenlivet_tags.rb
+++ app/overrides/glenlivet_tags.rb
@@ -1,28 +1,42 @@
-# Insert on Glenlivet brand page header
+# Insert on Glenlivet brand page (header)
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "floodlight_glenlivet_brand_header",
                      :insert_top => "head",
                      :partial => "spree/other_tags/glenlivet/brand_header",
                      :disabled => false)
 
-# Insert on Glenlivet brand page footer
+# Insert on Glenlivet brand page (footer)
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "floodlight_glenlivet_brand_footer",
                      :insert_bottom => "body",
                      :partial => "spree/other_tags/glenlivet/brand_footer",
                      :disabled => false)
 
-# Insert on Glenlivet product pages header
+# Insert on Glenlivet product pages (header)
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "floodlight_glenlivet_products_header",
                      :insert_top => "head",
                      :partial => "spree/other_tags/glenlivet/products_header",
                      :disabled => false)
 
-# Insert on Glenlivet product pages footer
+# Insert on Glenlivet product pages (footer)
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "floodlight_glenlivet_products_footer",
                      :insert_bottom => "body",
                      :partial => "spree/other_tags/glenlivet/products_footer",
+                     :disabled => false)
+
+# Insert on Cart page, if there are Glenlivet products (header)
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "floodlight_glenlivet_cart_header",
+                     :insert_top => "head",
+                     :partial => "spree/other_tags/glenlivet/cart_header",
+                     :disabled => false)
+
+# Insert on Cart page, if there are Glenlivet products (footer)
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "floodlight_glenlivet_cart_footer",
+                     :insert_bottom => "body",
+                     :partial => "spree/other_tags/glenlivet/cart_footer",
                      :disabled => false)
 

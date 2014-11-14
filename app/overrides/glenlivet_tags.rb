31170161d09e15df45b7211f99a82a0325e7e38e
Jason Knebel <jasonkneb@gmail.com>
2014-11-14 13:23:46 +0100

    adding tags to Glenlivet product pages

--- app/overrides/glenlivet_tags.rb
+++ app/overrides/glenlivet_tags.rb
@@ -12,3 +12,17 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :partial => "spree/other_tags/glenlivet/brand_footer",
                      :disabled => false)
 
+# Insert on Glenlivet product pages header
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "floodlight_glenlivet_products_header",
+                     :insert_top => "head",
+                     :partial => "spree/other_tags/glenlivet/products_header",
+                     :disabled => false)
+
+# Insert on Glenlivet product pages footer
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+                     :name => "floodlight_glenlivet_products_footer",
+                     :insert_bottom => "body",
+                     :partial => "spree/other_tags/glenlivet/products_footer",
+                     :disabled => false)
+

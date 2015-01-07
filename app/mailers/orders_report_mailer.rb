01fe5616bd2642fb31aa436d835152f0396862c6
Jason Knebel <jasonkneb@gmail.com>
2015-01-07 18:42:55 -0500

    improving performance of orders report

--- app/mailers/orders_report_mailer.rb
+++ app/mailers/orders_report_mailer.rb
@@ -6,7 +6,8 @@ class OrdersReportMailer < ActionMailer::Base
 
   def send_report(order_ids, user_id)
     @current_user = Spree::User.find user_id
-    @orders = Spree::Order.where(id: order_ids)
+    @orders = Spree::Order.where(id: order_ids).includes(:line_items,
+      :retailers, :profit_and_loss)
     @orders.each do |order|
       order.build_profit_and_loss if order.profit_and_loss.nil?
     end
@@ -22,69 +23,76 @@ class OrdersReportMailer < ActionMailer::Base
   end
 
   def admin_report
-    column_names = ["Order Number",
-                    "Order Date",
-                    "Accepted Date",
-                    "Product Name",
-                    "Number of Bottles",
-                    "Website Product Price",
-                    "Total Bottle Price",
-                    "Gift Packaging Charge (not paid to retailer)",
-                    "Gift Packaging Cost",
-                    "Corrugated Cost",
-                    "Shipping Charge",
-                    "State Fulfillment Fee",
-                    "Tax",
-                    "Promo discount($)",
-                    "Order Total",
-                    "Retail Bottle Price",
-                    "Credit Card Fees",
-                    "Total Disbursement To Retailer (product cost + sales tax + cc fees)",
-                    "Promo name",
-                    "Groupon Code",
-                    "Order Status",
-                    "Payment Status",
-                    "Shipment Status",
-                    "Retailer",
-                    "Ship-to State",
-                    "Customer email address"]
+    column_names = [
+      "Order Number",
+      "Order Date",
+      "Accepted Date",
+      "Product Name",
+      "Number of Bottles",
+      "Website Product Price",
+      "Total Bottle Price",
+      "Gift Packaging Charge (not paid to retailer)",
+      "Gift Packaging Cost",
+      "Corrugated Cost",
+      "Shipping Charge",
+      "State Fulfillment Fee",
+      "Tax",
+      "Promo discount($)",
+      "Order Total",
+      "Retail Bottle Price",
+      "Credit Card Fees",
+      "Total Disbursement To Retailer (product cost + sales tax + cc fees)",
+      "Promo name",
+      "Groupon Code",
+      "Order Status",
+      "Payment Status",
+      "Shipment Status",
+      "Retailer",
+      "Ship-to State",
+      "Customer email address"
+    ]
 
     CSV.generate do |csv|
       csv << column_names
 
       @orders.each do |order|
-        names_array = order.line_items.map{|line_item|line_item.product.try(:name)}.compact
-        prices_array = order.line_items.map{|line_item|line_item.price}.compact
+        line_items = order.line_items
+        profit_and_loss = order.profit_and_loss
+        retailer = order.retailer
+
+        names_array = line_items.map{|line_item|line_item.product.try(:name)}.compact
+        prices_array = line_items.map{|line_item|line_item.price}.compact
         prices_array = prices_array.map { |p| number_to_currency(p) } if prices_array.present?
         shipping_charge_uplift = order.shipping_method.calculator.preferred_uplift rescue 0.0
 
-        csv << [order.number,
-                (order.completed_at.nil? ? order.created_at : order.completed_at).to_date,
-                order.accepted_at.nil? ? nil : order.accepted_at.to_date,
-                names_array.empty? ? nil : strip_tags(names_array.join('|')).gsub(/&quot;|,/, ''),
-                order.number_of_bottles,
-                prices_array.empty? ? nil : prices_array.join('|'),
-                number_to_currency(order.profit_and_loss.total_bottle_price),
-                number_to_currency(order.profit_and_loss.gift_packaging_charge),
-                number_to_currency(order.profit_and_loss.gift_packaging_cost),
-                number_to_currency(order.profit_and_loss.corrugated_cost),
-                number_to_currency(order.profit_and_loss.shipping_costs),
-                number_to_currency(order.profit_and_loss.state_fulfillment_fee),
-                number_to_currency(order.profit_and_loss.sales_tax),
-                number_to_currency(order.profit_and_loss.promotions),
-                number_to_currency(order.total),
-                number_to_currency(order.profit_and_loss.retailer_bottle_price),
-                number_to_currency(order.profit_and_loss.credit_card_fees),
-                order.retailer ? number_to_currency(order.profit_and_loss.net_retailer_disbursements) : number_to_currency(0.00),
-                order.adjustments.eligible.promotion.first.try(:label),
-                order.try(:groupon_code).try(:code),
-                order.state,
-                order.payment_state,
-                order.shipment_state,
-                order.retailer ? order.retailer.name : '',
-                order.ship_address ? order.ship_address.state.abbr : '',
-                order.email
-          ]
+        csv << [
+          order.number,
+          (order.completed_at.nil? ? order.created_at : order.completed_at).to_date,
+          order.accepted_at.nil? ? nil : order.accepted_at.to_date,
+          names_array.empty? ? nil : strip_tags(names_array.join('|')).gsub(/&quot;|,/, ''),
+          order.number_of_bottles,
+          prices_array.empty? ? nil : prices_array.join('|'),
+          number_to_currency(profit_and_loss.total_bottle_price),
+          number_to_currency(profit_and_loss.gift_packaging_charge),
+          number_to_currency(profit_and_loss.gift_packaging_cost),
+          number_to_currency(profit_and_loss.corrugated_cost),
+          number_to_currency(profit_and_loss.shipping_costs),
+          number_to_currency(profit_and_loss.state_fulfillment_fee),
+          number_to_currency(profit_and_loss.sales_tax),
+          number_to_currency(profit_and_loss.promotions),
+          number_to_currency(order.total),
+          number_to_currency(profit_and_loss.retailer_bottle_price),
+          number_to_currency(profit_and_loss.credit_card_fees),
+          retailer ? number_to_currency(profit_and_loss.net_retailer_disbursements) : number_to_currency(0.00),
+          order.adjustments.eligible.promotion.first.try(:label),
+          order.try(:groupon_code).try(:code),
+          order.state,
+          order.payment_state,
+          order.shipment_state,
+          retailer ? retailer.name : '',
+          order.ship_address ? order.ship_address.state.abbr : '',
+          order.email
+        ]
       end
     end
 
@@ -104,7 +112,7 @@ class OrdersReportMailer < ActionMailer::Base
             order.payment_state,
             order.shipment_state,
             number_to_currency(order.tax_total),
-            number_to_currency((order.line_items.collect {|line_item| line_item.product_cost_for_retailer }).sum),
+            number_to_currency((line_items.collect {|line_item| line_item.product_cost_for_retailer }).sum),
             order.retailer ? number_to_currency(order.total_amount_due_to_retailer) : number_to_currency(0.00)
           ]
       end

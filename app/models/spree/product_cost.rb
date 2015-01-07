85b4bb42af929b71aea17b1cbb19ff47e392e610
Jason Knebel <jasonkneb@gmail.com>
2015-01-07 14:43:42 -0500

    ProductCost import now created new, if none found

--- app/models/spree/product_cost.rb
+++ app/models/spree/product_cost.rb
@@ -6,7 +6,6 @@ module Spree
     belongs_to :retailer
     belongs_to :variant
 
-
     def shipping_surcharge_is_not_zero
       where('shipping_surcharge_is_not_zero > 0')
     end
@@ -22,20 +21,30 @@ module Spree
           begin
             retailer = Spree::Retailer.find_by_name(h["retailer_name"])
             variant  = Spree::Variant.where(deleted_at: nil).find_by_sku(h["sku"])
-            product_cost = Spree::ProductCost.find_by_retailer_id_and_variant_id(retailer.id, variant.id) if retailer.present? && variant.present?
 
-            if product_cost.present?
-              product_cost.cost_price         = h["cost"].to_f               if h["cost"].present?
-              product_cost.shipping_surcharge = h["shipping_surcharge"].to_f if h["shipping_surcharge"].present?
-              product_cost.fulfillment_fee    = h["fulfillment_fee"].to_f    if h["fulfillment_fee"].present?
-              product_cost.save
-            else
+            unless retailer.present? && variant.present?
               log += "Row #{i}: Cannot find retailer -- #{h['retailer_name']}<br />" unless retailer.present?
               log += "Row #{i}: Cannot find product sku -- #{h['sku']}<br />" unless variant.present?
-              log += "Row #{i}: Cannot find product cost for #{retailer.name} and #{variant.sku}<br />" if retailer.present? && variant.present?
+            else
+              product_cost = Spree::ProductCost.find_by_retailer_id_and_variant_id(retailer.id, variant.id)
+
+              if product_cost.present?
+                product_cost.cost_price         = h["cost"].to_f               if h["cost"].present?
+                product_cost.shipping_surcharge = h["shipping_surcharge"].to_f if h["shipping_surcharge"].present?
+                product_cost.fulfillment_fee    = h["fulfillment_fee"].to_f    if h["fulfillment_fee"].present?
+                product_cost.save
+              else
+                product_cost = Spree::ProductCost.new
+                product_cost.variant_id         = variant.id
+                product_cost.retailer_id        = retailer.id
+                product_cost.cost_price         = h["cost"].to_f               if h["cost"].present?
+                product_cost.shipping_surcharge = h["shipping_surcharge"].to_f if h["shipping_surcharge"].present?
+                product_cost.fulfillment_fee    = h["fulfillment_fee"].to_f    if h["fulfillment_fee"].present?
+                product_cost.save
+              end
             end
           rescue => e
-            Rails.log e
+            log = e
           end
         end
       end

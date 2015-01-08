4ff4d798a9c23abeb15f46dab21e6f2c42ea93e8
Jason Knebel <jasonkneb@gmail.com>
2015-01-07 19:03:34 -0500

    improving performance for product sales report

--- app/mailers/product_sales_report_mailer.rb
+++ app/mailers/product_sales_report_mailer.rb
@@ -6,7 +6,8 @@ class ProductSalesReportMailer < ActionMailer::Base
 
   def send_report(order_ids, user_id)
     @current_user = Spree::User.find user_id
-    @orders = Spree::Order.where(id: order_ids)
+    @orders = Spree::Order.where(id: order_ids).includes(:line_items,
+      :retailers, :ship_address)
 
     attachments["product_sales_report.csv"] = { :mime_type => 'text/csv', :content => report_csv_file }
     mail(:to => @current_user.email, :reply_to => "noreply@reservebar.com", :subject => "Your product sales report is ready.")
@@ -19,7 +20,29 @@ class ProductSalesReportMailer < ActionMailer::Base
   end
 
   def admin_report
-    column_names = ["OrderNumber", "Order details link", "Customer email address", "Ship-to State", "Product name", "Partner", "Brand", "Number of bottles", "Multiple products in the order", "OrderDate", "AcceptedDate", "OrderState", "PaymentState", "ShipmentState", "Product price", "GiftPackagingCost(not paid to retailer)", "RB margin (Product price - product cost)", "Promo", "Total promo discount($)", "Retailer", "ProductCostForRetailer"]
+    column_names = [
+      "OrderNumber",
+      "Order details link",
+      "Customer email address",
+      "Ship-to State",
+      "Product name",
+      "Partner",
+      "Brand",
+      "Number of bottles",
+      "Multiple products in the order",
+      "OrderDate",
+      "AcceptedDate",
+      "OrderState",
+      "PaymentState",
+      "ShipmentState",
+      "Product price",
+      "GiftPackagingCost(not paid to retailer)",
+      "RB margin (Product price - product cost)",
+      "Promo",
+      "Total promo discount($)",
+      "Retailer",
+      "ProductCostForRetailer"
+    ]
 
     CSV.generate do |csv|
       csv << column_names
@@ -27,28 +50,29 @@ class ProductSalesReportMailer < ActionMailer::Base
       @orders.each do |order|
         line_items = order.line_items
         line_items.each do |line_item|
-          csv << [order.number,
-            spree.order_url(order, :protocol => 'https'),
-            order.email,
-            order.ship_address.state.abbr,
-            line_item.product.nil? ? nil : strip_tags(line_item.product.name).gsub(/&quot;|,/, ''),
-            line_item.product.nil? ? nil : line_item.product.partner.try(:name),
-            line_item.product.nil? ? nil : line_item.product.brand.title,
-            line_item.quantity,
-            (line_items.size > 1 ? "Yes" : "No"),
-            (@show_only_completed ? order.completed_at : order.created_at).to_date,
-            order.accepted_at.nil? ? nil : order.accepted_at.to_date,
-            order.present? ? order.state : nil,
-            order.payment_state,
-            order.shipment_state,
-            number_to_currency(line_item.price),
-            number_to_currency(line_item.adjustments.eligible.gift_packaging.map(&:amount).sum),
-            number_to_currency(line_item.margin_for_site),
-            order.adjustments.eligible.promotion.first.try(:label),
-            number_to_currency(order.adjustments.eligible.promotion.first.try(:amount)),
-            order.retailer.try(:name),
-            number_to_currency(line_item.product_cost_for_retailer)
-          ]
+        csv << [
+          order.number,
+          spree.order_url(order, :protocol => 'https'),
+          order.email,
+          order.ship_address.state.abbr,
+          line_item.product.nil? ? nil : strip_tags(line_item.product.name).gsub(/&quot;|,/, ''),
+          line_item.product.nil? ? nil : line_item.product.partner.try(:name),
+          line_item.product.nil? ? nil : line_item.product.brand.title,
+          line_item.quantity,
+          (line_items.size > 1 ? "Yes" : "No"),
+          (@show_only_completed ? order.completed_at : order.created_at).to_date,
+          order.accepted_at.nil? ? nil : order.accepted_at.to_date,
+          order.present? ? order.state : nil,
+          order.payment_state,
+          order.shipment_state,
+          number_to_currency(line_item.price),
+          number_to_currency(line_item.adjustments.eligible.gift_packaging.map(&:amount).sum),
+          number_to_currency(line_item.margin_for_site),
+          order.adjustments.eligible.promotion.first.try(:label),
+          number_to_currency(order.adjustments.eligible.promotion.first.try(:amount)),
+          order.retailer.try(:name),
+          number_to_currency(line_item.product_cost_for_retailer)
+        ]
         end
       end
     end

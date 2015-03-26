6c589774a076d74fc5511d08bdbe551cfceaabf1
Jason Knebel <jasonkneb@gmail.com>
2015-03-26 16:36:44 -0400

    adding Grand Marnier tags to product pages

--- app/overrides/floodlight_grand_marnier.rb
+++ app/overrides/floodlight_grand_marnier.rb
@@ -1,6 +1,17 @@
 # Insert on Grand Marnier brand page
-Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
-                     :name => "floodlight_grand_marnier_brand",
-                     :insert_top => "body",
-                     :partial => "spree/floodlight_tags/grand_marnier/brand",
-                     :disabled => false)
+Deface::Override.new(
+  virtual_path: "spree/layouts/spree_application",
+  name: "floodlight_grand_marnier_brand",
+  insert_top: "body",
+  partial: "spree/floodlight_tags/grand_marnier/brand",
+  disabled: false
+)
+
+# Insert on Grand Marnier product pages
+Deface::Override.new(
+  virtual_path: "spree/layouts/spree_application",
+  name: "floodlight_grand_marnier_products",
+  insert_top: "body",
+  partial: "spree/floodlight_tags/grand_marnier/products",
+  disabled: false
+)

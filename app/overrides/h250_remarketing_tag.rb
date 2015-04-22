fdf6cf740ffce939c3a17e3f510986a02071d689
Jason Knebel <jasonkneb@gmail.com>
2015-04-22 17:17:58 -0400

    adding H250 re-marketing tags

---
+++ app/overrides/h250_remarketing_tag.rb
@@ -0,0 +1,8 @@
+# H250 Product Remarketing Tag (22 Apr 2015)
+Deface::Override.new(
+  virtual_path: "spree/layouts/spree_application",
+  name: "h250_remarketing_tag",
+  insert_bottom: "body",
+  partial: "spree/other_tags/h250_remarketing_tag",
+  disabled: false
+)

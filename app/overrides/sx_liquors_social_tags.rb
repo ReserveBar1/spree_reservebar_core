633439a5bc056a9cea15cdf3f3a253408e07dd32
Jason Knebel <jasonkneb@gmail.com>
2015-05-05 17:52:48 -0400

    adding SX liquors FB tag to confirmation page

---
+++ app/overrides/sx_liquors_social_tags.rb
@@ -0,0 +1,8 @@
+# Insert on SX Liquors Confirmation page
+Deface::Override.new(
+  virtual_path: "spree/layouts/spree_application",
+  name: "social_sx_liquors_confirmation",
+  insert_top: "body",
+  partial: "spree/other_tags/sx_liquors_social_tags",
+  disabled: false
+)

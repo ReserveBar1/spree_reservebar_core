80730fdfc38c2e29fb1967b923149c38154db74e
Jason Knebel <jasonkneb@gmail.com>
2015-05-28 16:14:56 -0400

    adding OAK floodlight/facebook address tags

--- app/overrides/oak_by_absolut_tags.rb
+++ app/overrides/oak_by_absolut_tags.rb
@@ -48,3 +48,19 @@ Deface::Override.new(
   partial: "spree/facebook_tags/oak_by_absolut/cart",
   disabled: false
 )
+
+# Insert on Address page, if there are OAK by Absolut products
+Deface::Override.new(
+  virtual_path: "spree/layouts/spree_application",
+  name: "floodlight_oak_by_absolut_address",
+  insert_top: "body",
+  partial: "spree/floodlight_tags/oak_by_absolut/address",
+  disabled: false
+)
+Deface::Override.new(
+  virtual_path: "spree/layouts/spree_application",
+  name: "facebook_oak_by_absolut_address",
+  insert_top: "head",
+  partial: "spree/facebook_tags/oak_by_absolut/address",
+  disabled: false
+)

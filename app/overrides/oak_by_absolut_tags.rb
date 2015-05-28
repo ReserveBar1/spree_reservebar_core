e911a35aa81b738e2e9bc97a32a557d7e005f0df
Jason Knebel <jasonkneb@gmail.com>
2015-05-28 16:04:07 -0400

    adding OAK floodlight/facebook product tags

--- app/overrides/oak_by_absolut_tags.rb
+++ app/overrides/oak_by_absolut_tags.rb
@@ -14,3 +14,20 @@ Deface::Override.new(
   partial: "spree/facebook_tags/oak_by_absolut/brand",
   disabled: false
 )
+
+# Insert on OAK by Absolut product page
+Deface::Override.new(
+  virtual_path: "spree/layouts/spree_application",
+  name: "floodlight_oak_by_absolut_product",
+  insert_top: "body",
+  partial: "spree/floodlight_tags/oak_by_absolut/product",
+  disabled: false
+)
+
+Deface::Override.new(
+  virtual_path: "spree/layouts/spree_application",
+  name: "facebook_oak_by_absolut_product",
+  insert_top: "head",
+  partial: "spree/facebook_tags/oak_by_absolut/product",
+  disabled: false
+)

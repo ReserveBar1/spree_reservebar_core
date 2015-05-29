221139968661ab283cab90d7c3c4bf0565dc119c
Jason Knebel <jasonkneb@gmail.com>
2015-05-29 14:31:04 -0400

    removing OAK brand tags for Absolut

--- app/overrides/oak_by_absolut_tags.rb
+++ app/overrides/oak_by_absolut_tags.rb
@@ -1,20 +1,3 @@
-# Insert on Absolut brand page
-Deface::Override.new(
-  virtual_path: "spree/layouts/spree_application",
-  name: "floodlight_oak_by_absolut_brand",
-  insert_top: "body",
-  partial: "spree/floodlight_tags/oak_by_absolut/brand",
-  disabled: false
-)
-
-Deface::Override.new(
-  virtual_path: "spree/layouts/spree_application",
-  name: "facebook_oak_by_absolut_brand",
-  insert_top: "head",
-  partial: "spree/facebook_tags/oak_by_absolut/brand",
-  disabled: false
-)
-
 # Insert on OAK by Absolut product page
 Deface::Override.new(
   virtual_path: "spree/layouts/spree_application",

3cb609344d4b94f8560910d61ce37dd7fdfce5dc
Jason Knebel <jasonkneb@gmail.com>
2015-06-04 13:53:20 -0400

    adding floodlight tag to brand page

---
+++ app/overrides/cruzan_tags.rb
@@ -0,0 +1,8 @@
+# Insert on Cruzan pages (logic in tag itself)
+Deface::Override.new(
+  virtual_path: "spree/layouts/spree_application",
+  name: "floodlight_cruzan",
+  insert_top: "body",
+  partial: "spree/floodlight_tags/cruzan_tag",
+  disabled: false
+)

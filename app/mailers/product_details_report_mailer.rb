dfb18cc58807acd8382c5c643dee63123f30682d
Jason Knebel <jasonkneb@gmail.com>
2015-06-09 17:46:50 -0400

    updating product details report

--- app/mailers/product_details_report_mailer.rb
+++ app/mailers/product_details_report_mailer.rb
@@ -1,10 +1,14 @@
 class ProductDetailsReportMailer < ActionMailer::Base
+  include ActionView::Helpers::SanitizeHelper
+  include ActionView::Helpers::NumberHelper
+
   default from: 'noreply@reservebar.com'
 
   def send_report(user_id)
     user = Spree::User.find user_id
     @variants = Spree::Variant.where(deleted_at: nil).includes(:product,
-      { product: :brand }, { product: { brand: :brand_owner } })
+      { product: :shipping_category }, { product: :brand },
+      { product: { brand: :brand_owner } })
 
     filename = "product_details_report_#{Time.now.strftime('%Y%m%d%H%M')}.csv"
     attachments[filename] = {
@@ -21,19 +25,45 @@ class ProductDetailsReportMailer < ActionMailer::Base
   private
 
   def report_csv_file
-    column_names = [ 'Brand Owner', 'Brand Name', 'Product Name',
-      'SKU','Weight' ]
+    column_names = [
+      'Product Name',
+      'Master Price',
+      'Available on',
+      'SKU',
+      'Weight',
+      'Height',
+      'Width',
+      'Depth',
+      'Shipping Category',
+      'Tax Category',
+      'Brand Owner',
+      'Brand',
+      'Partner',
+      'State Blacklist'
+    ]
 
     CSV.generate do |csv|
       csv << column_names
 
       @variants.each do |var|
+        available_on = var.product.try(:available_on)
+        partner_id = var.product.try(:partner_id)
+
         csv << [
+          var.product.try(:name),
+          number_to_currency(var.price),
+          available_on.present? ? available_on.strftime('%Y-%m-%d') : nil,
+          var.sku,
+          var.weight.to_f,
+          var.height.to_f,
+          var.width.to_f,
+          var.depth.to_f,
+          var.product.try(:shipping_category).try(:name),
+          var.product.try(:tax_category).try(:name),
           var.product.try(:brand).try(:brand_owner).try(:title),
           var.product.try(:brand).try(:title),
-          var.product.try(:name),
-          var.sku,
-          var.weight
+          partner_id.present? ? Partner.find(partner_id).name : nil,
+          var.product.try(:state_blacklist)
         ]
       end
     end

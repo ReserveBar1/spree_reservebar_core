795e74bf5e4ad1bd722d5780d0449a4b6becccce
Jason Knebel <jasonkneb@gmail.com>
2015-09-09 17:06:43 -0400

    handling ardbeg bottle count outside of the cart

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -3,15 +3,15 @@ Spree::OrdersController.class_eval do
 
   before_filter :check_bottle_number_limit, :only => [:populate, :update]
   before_filter :check_ardbeg_bottle_number_limit, :only => [:populate, :update]
-  
+
   rescue_from Exceptions::BottleLimitPerOrderExceededError, :with => :bottle_limit_exceeded
   rescue_from Exceptions::ArdbegBottleLimitPerOrderExceededError, with: :ardbeg_bottle_limit_exceeded
-  
+
   # Add customization to line item after the other stuff 
   after_filter :handle_customization, :only => [:populate, :update]
-  
+
   protected
-  
+
   # We'll need to add customization data to the SKU's either when it was submitted in the form or when the SKU requires it but it has not been 
   # submitted. The latter happens when SKUis added to the cart from a taxon page, where the form data is not available.
   def handle_customization
@@ -39,31 +39,30 @@ Spree::OrdersController.class_eval do
       end        
     end
   end
-  
+
   def bottle_limit_exceeded
     respond_to do |format|
       format.html {
-        flash[:notice] = "<p>Thank you for trying to add products to your shopping cart. We appreciate your business; however,  we currently cannot accept an order that contains more than 12 bottles. </p>
-        <p>If you wish to purchase more than 12 bottles (e.g., more than one case of wine), please create separate orders, each of which must contain 12 or fewer bottles. We apologize for the inconvenience.</p>".html_safe
-          redirect_to cart_path
-        }
-        format.js {
-          render :populate_bottle_limit_exceeded
-        }
-      end
+        flash[:notice] = "<p>Thank you for trying to add products to your shopping cart. We appreciate your business; however,  we currently cannot accept an order that contains more than 12 bottles. </p><p>If you wish to purchase more than 12 bottles (e.g., more than one case of wine), please create separate orders, each of which must contain 12 or fewer bottles. We apologize for the inconvenience.</p>".html_safe
+        redirect_to cart_path
+      }
+      format.js {
+        render :populate_bottle_limit_exceeded
+      }
     end
+  end
 
-    def ardbeg_bottle_limit_exceeded
-      respond_to do |format|
-        format.html {
-          flash[:notice] = "<p>Thank you for trying to add products to your shopping cart. We appreciate your business; however,  we currently cannot accept an order that contains more than 1 bottles of Ardbeg Supernova 2015.</p>".html_safe
-          redirect_to cart_path
-        }
-        format.js {
-          render :populate_bottle_limit_exceeded
-        }
-      end
+  def ardbeg_bottle_limit_exceeded
+    respond_to do |format|
+      format.html {
+        flash[:notice] = "<p>Thank you for trying to add products to your shopping cart. We appreciate your business; however,  we currently cannot accept an order that contains more than 1 bottle of Ardbeg Supernova 2015.</p>".html_safe
+        redirect_to cart_path
+      }
+      format.js {
+        render :populate_ardbeg_bottle_limit_exceeded
+      }
     end
+  end
 
   # Do not allow the user to add more than 12 bottles to the order.
   # If he tries, show him a message instead of allowing it
@@ -102,15 +101,23 @@ Spree::OrdersController.class_eval do
     if @order.line_items.map(&:product).map(&:permalink).include?('ardbeg-supernova-2015')
       @order.line_items.each do |li|
         if li.product.permalink == 'ardbeg-supernova-2015'
-          if li.quantity > max_bottles
-            raise Exceptions::ArdbegBottleLimitPerOrderExceededError
-          elsif params[:order] && params[:order][:line_items_attributes]
-            params[:order][:line_items_attributes].each do |index, attributes|
-              if attributes['id'].to_i == li.id and attributes['quantity'].to_i > max_bottles
-                li.update_attributes(quantity: max_bottles)
-                raise Exceptions::ArdbegBottleLimitPerOrderExceededError
-              end
+          desired_bottles = li.quantity
+
+          if params[:variants].present?
+            # updating from product#index (search) or custom page
+            params[:variants].each do |variant, quantity|
+              desired_bottles += quantity.to_i if variant.to_i == li.variant_id
             end
+          elsif params[:order] && params[:order][:line_items_attributes]
+            # updating from cart
+            params[:order][:line_items_attributes].each do |index, attributes|
+              desired_bottles += attributes['quantity'].to_i if attributes['id'].to_i == li.id
+            end
+          end
+
+          if desired_bottles > max_bottles
+            li.update_attributes(quantity: max_bottles)
+            raise Exceptions::ArdbegBottleLimitPerOrderExceededError
           end
         end
       end

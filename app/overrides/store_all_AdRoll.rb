3be781002d5e107e2a49f9bc442111ec60b23285
Jason Knebel <jasonkneb@gmail.com>
2015-10-01 16:35:16 -0400

    adding AdRoll tag

--- app/overrides/store_all_AdRoll.rb
+++ app/overrides/store_all_AdRoll.rb
@@ -1,9 +1,9 @@
 # Insert on all pages
 Deface::Override.new(
   virtual_path: "spree/layouts/spree_application",
-  name: "smarter_pixel",
-  sequence: 1000,
+  name: "adroll",
+  sequence: 1001, # insert after smarter pixels
   insert_bottom: "body",
-  partial: "spree/smarter_pixel/all_pages",
+  partial: "spree/shared/adroll",
   disabled: false
 )

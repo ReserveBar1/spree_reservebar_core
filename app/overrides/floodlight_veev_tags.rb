a162560be41b4b622433728f3e04ceaf262cee8d
Jason Knebel <jasonkneb@gmail.com>
2015-10-30 11:46:41 -0400

    disabling VEEV floodlight tags

--- app/overrides/floodlight_veev_tags.rb
+++ app/overrides/floodlight_veev_tags.rb
@@ -3,18 +3,18 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "floodlight_veev_brand",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/veev/brand",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on VEEV product pages
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "floodlight_veev_products",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/veev/products",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on Confirmation page, if there are VEEV products
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "floodlight_veev_confirmation",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/veev/confirmation",
-                     :disabled => false)
+                     :disabled => true)

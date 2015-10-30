9f55cf74349e3615abee4f96c7b6756b6f602897
Jason Knebel <jasonkneb@gmail.com>
2015-10-30 11:44:13 -0400

    disabling Diageo Floodlight tags

--- app/overrides/floodlight_diageo_tags.rb
+++ app/overrides/floodlight_diageo_tags.rb
@@ -10,18 +10,18 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "floodlight_diageo_order_summary",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/diageo/orders_show",
-                     :disabled => false)
+                     :disabled => true)
                    
 # Insert on taxon page - taxon dependency is handled in the partial
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "floodlight_diageo_taxon_show",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/diageo/taxon_show",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on product page
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "floodlight_diageo_product_show",
                      :insert_top => "body",
                      :partial => "spree/floodlight_tags/diageo/product_show",
-                     :disabled => false)
+                     :disabled => true)

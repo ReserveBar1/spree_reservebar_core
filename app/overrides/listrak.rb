2325eaca1676b12cbaa27085c29025acbce25016
Jason Knebel <jasonkneb@gmail.com>
2015-11-04 00:16:54 -0500

    sending order completion information to Listrak

--- app/overrides/listrak.rb
+++ app/overrides/listrak.rb
@@ -7,3 +7,22 @@ Deface::Override.new(
   sequence: 9999, # done last
   disabled: false
 )
+
+# Listrak Order Confirmation
+Deface::Override.new(
+  virtual_path: 'spree/layouts/spree_application',
+  name: 'listrak_order_confirmation',
+  insert_top: 'body',
+  partial: 'spree/listrak/order_confirmation',
+  disabled: false
+)
+
+# Listrak Cart Completed, if no JS
+Deface::Override.new(
+  virtual_path: 'spree/layouts/spree_application',
+  name: 'listrak_no_js_order_confirmation',
+  insert_top: 'body',
+  partial: 'spree/listrak/no_js_order_confirmation',
+  sequence: 10,
+  disabled: false
+)

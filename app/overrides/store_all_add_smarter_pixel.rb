f0b19a45802ebb9c3960e36e7826aec8c7ea4bdf
jfields <justin@railsdog.com>
2015-11-10 14:57:00 -0800

    Removing steelhouse tracker script and override.

--- app/overrides/store_all_add_smarter_pixel.rb
+++
@@ -1,9 +0,0 @@
-# Insert on all pages
-Deface::Override.new(
-  virtual_path: "spree/layouts/spree_application",
-  name: "smarter_pixel",
-  sequence: 1000,
-  insert_bottom: "body",
-  partial: "spree/smarter_pixel/all_pages",
-  disabled: false
-)

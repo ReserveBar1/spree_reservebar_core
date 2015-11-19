0d4f4becd0556161948212acf02d7d30aff8f0ba
Jason Knebel <jasonkneb@gmail.com>
2015-11-19 15:39:09 -0500

    removing Hornitos Facebook tags

--- app/overrides/facebook_hornitos.rb
+++ app/overrides/facebook_hornitos.rb
@@ -3,39 +3,39 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "facebook_hornitos_brand",
                      :insert_top => "body",
                      :partial => "spree/facebook_tags/hornitos/brand",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on Hornitos product pages
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "facebook_hornitos_products",
                      :insert_top => "body",
                      :partial => "spree/facebook_tags/hornitos/products",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on Cart page, if there are Hornitos products
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "facebook_hornitos_cart",
                      :insert_top => "body",
                      :partial => "spree/facebook_tags/hornitos/cart",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on Registration page, if there are Hornitos products in order
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "facebook_hornitos_registration",
                      :insert_top => "body",
                      :partial => "spree/facebook_tags/hornitos/registration",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on Address page, if there are Hornitos products
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "facebook_hornitos_address",
                      :insert_top => "body",
                      :partial => "spree/facebook_tags/hornitos/address",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on Confirmation page, if there are Hornitos products
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "facebook_hornitos_confirmation",
                      :insert_top => "body",
                      :partial => "spree/facebook_tags/hornitos/confirmation",
-                     :disabled => false)
+                     :disabled => true)

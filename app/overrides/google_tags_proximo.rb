a90fd3243c9cc144b932016e56ef9bb524a6bea2
Jason Knebel <jasonkneb@gmail.com>
2015-11-19 15:44:30 -0500

    removing Proximo google tags

--- app/overrides/google_tags_proximo.rb
+++ app/overrides/google_tags_proximo.rb
@@ -3,32 +3,32 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "google_proximo_brand",
                      :insert_top => "body",
                      :partial => "spree/google_tags/proximo/brand",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on Proximo product pages
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "google_proximo_products",
                      :insert_top => "body",
                      :partial => "spree/google_tags/proximo/products",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on Cart page, if there are Proximo products
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "google_proximo_cart",
                      :insert_top => "body",
                      :partial => "spree/google_tags/proximo/cart",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on Address page, if there are Proximo products
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "google_proximo_address",
                      :insert_top => "body",
                      :partial => "spree/google_tags/proximo/address",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on Confirmation page, if there are Proximo products
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "google_proximo_confirmation",
                      :insert_top => "body",
                      :partial => "spree/google_tags/proximo/confirmation",
-                     :disabled => false)
+                     :disabled => true)

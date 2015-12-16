491c1673e81eecb87d99e5c094d68279bbcdac5f
Jason Knebel <jasonkneb@gmail.com>
2015-12-15 21:05:47 -0500

    reenabling christmas delivery deadlines

--- app/overrides/checkout_christmas_delivery.rb
+++ app/overrides/checkout_christmas_delivery.rb
@@ -2,4 +2,4 @@ Deface::Override.new(:virtual_path => "spree/checkout/edit",
                      :name => "christmas_delivery",
                      :insert_after => "code[erb-loud]:contains('checkout_form_')",
                      :partial => 'spree/checkout/christmas_delivery',
-                     :disabled => true)
+                     :disabled => false)

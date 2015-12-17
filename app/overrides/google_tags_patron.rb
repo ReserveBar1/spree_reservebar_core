143977d9a522fb80602542909519c00a2ee64ab8
Jason Knebel <jasonkneb@gmail.com>
2015-12-17 12:14:03 -0500

    reenabling Patron google tags

--- app/overrides/google_tags_patron.rb
+++ app/overrides/google_tags_patron.rb
@@ -3,25 +3,25 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "google_patron_brand",
                      :insert_bottom => "body",
                      :partial => "spree/google_tags/patron/brand",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on Patron product pages
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "google_patron_products",
                      :insert_bottom => "body",
                      :partial => "spree/google_tags/patron/products",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on Cart page, if there are Patron products
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "google_patron_cart",
                      :insert_top => "body",
                      :partial => "spree/google_tags/patron/cart",
-                     :disabled => false)
+                     :disabled => true)
 
 # Insert on Confirmation page, if there are Patron products
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "google_patron_confirmation",
                      :insert_top => "body",
                      :partial => "spree/google_tags/patron/confirmation",
-                     :disabled => false)
+                     :disabled => true)

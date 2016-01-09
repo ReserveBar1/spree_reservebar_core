2dfc971e95135db204c1a22dc237f542a72fa8f4
Jason Knebel <jasonkneb@gmail.com>
2016-01-08 21:27:29 -0500

    changing email recipients for tiffany concierge

--- app/mailers/concierge_mailer.rb
+++ app/mailers/concierge_mailer.rb
@@ -2,11 +2,20 @@ class ConciergeMailer < ActionMailer::Base
   default from: "noreply@reservebar.com"
 
   def send_email(data, brand_name)
-    mail(
-      to: 'nflanagan@reservebar.com, slesser@reservebar.com',
-      subject: "#{brand_name} Email Submitted",
-      body: data
-    )
+    if brand_name.downcase == 'tiffany'
+      mail(
+        to: 'danf@reservebar.com',
+        cc: 'nflanagan@reservebar.com',
+        subject: "#{brand_name} Email Submitted",
+        body: data
+      )
+    else
+      mail(
+        to: 'nflanagan@reservebar.com, slesser@reservebar.com',
+        subject: "#{brand_name} Email Submitted",
+        body: data
+      )
+    end
   end
 
 end

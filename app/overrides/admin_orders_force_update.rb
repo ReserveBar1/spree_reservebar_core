1e6ab242d5e4f0602e3579a6a11c7bf131d2a72f
Jason Knebel <jasonkneb@gmail.com>
2016-03-22 23:41:42 -0400

    giving unique name to deface for force update

--- app/overrides/admin_orders_force_update.rb
+++ app/overrides/admin_orders_force_update.rb
@@ -1,5 +1,7 @@
-Deface::Override.new(:virtual_path => "spree/admin/orders/edit",
-                     :name => "admin_orders_change_retailer",
-                     :insert_bottom => "#order-form-wrapper",
-                     :partial => "spree/admin/orders/force_update",
-                     :disabled => false)
+Deface::Override.new(
+  virtual_path: "spree/admin/orders/edit",
+  name: "admin_orders_force_update",
+  insert_bottom: "#order-form-wrapper",
+  partial: "spree/admin/orders/force_update",
+  disabled: false
+)

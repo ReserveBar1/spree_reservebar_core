3d912e1370b81d59969fdb400e3245ba75ef6169
Jason Knebel <jasonkneb@gmail.com>
2016-05-17 17:06:24 -0400

    adding Avion products floodlight tag

--- app/overrides/floodlight_pernod_ricard.rb
+++ app/overrides/floodlight_pernod_ricard.rb
@@ -1,3 +1,12 @@
+# Insert on Product page, if there are Avion products
+Deface::Override.new(
+  virtual_path: "spree/layouts/spree_application",
+  name: "floodlight_pernod_ricard_avion",
+  insert_top: "body",
+  partial: "spree/floodlight_tags/pernod_ricard/avion",
+  disabled: false
+)
+
 # Insert on Confirmation page, if there are Pernod Ricard products
 Deface::Override.new(
   virtual_path: "spree/layouts/spree_application",
